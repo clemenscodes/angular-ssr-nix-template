903dc41663b10e09a53cdd3875de235045b68922
Clemens Horn <clemens.horn@mni.thm.de>
2025-05-24 18:07:00 +0200

    fix(template): e2e playwright setup

--- apps/template-e2e/playwright.config.ts
+++ apps/template-e2e/playwright.config.ts
@@ -32,37 +32,21 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          ...(process.env['PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH']
+            ? {
+                executablePath:
+                  process.env['PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH'],
+              }
+            : {}),
+        },
+      },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    // Uncomment for mobile browsers support
-    /* {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    }, */
-
-    // Uncomment for branded browsers
-    /* {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    } */
   ],
 });
--- shell.nix
+++ shell.nix
@@ -1,8 +1,25 @@
-{callPackage}: let
+{
+  callPackage,
+  playwright-driver,
+  nodejs_20,
+  jq,
+}: let
   mainPkg = callPackage ./default.nix {};
 in
-  mainPkg.overrideAttrs (_: {
+  mainPkg.overrideAttrs (oa: {
+    nativeBuildInputs =
+      [
+        playwright-driver.browsers
+        jq
+      ]
+      ++ (oa.nativeBuildInputs or []);
+
     shellHook = ''
+      playwright_chromium_revision=$(${jq}/bin/jq --raw-output '.browsers[] | select(.name == "chromium").revision' ${playwright-driver}/browsers.json)
+      export PLAYWRIGHT_NODEJS_PATH=${nodejs_20}/bin/node
+      export PLAYWRIGHT_BROWSERS_PATH=${playwright-driver.browsers}
+      export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+      export PLAYWRIGHT_LAUNCH_OPTIONS_EXECUTABLE_PATH=${playwright-driver.browsers}/chromium-$playwright_chromium_revision/chrome-linux/chrome
       export PATH="./node_modules/.bin:$PATH"
       echo "Angular devShell loaded..."
     '';
